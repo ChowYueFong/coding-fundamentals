7c18e875d99e2ec0c00cb983e16aef74a119e7be
ChowYueFong <122257040+ChowYueFong@users.noreply.github.com>
2023-05-24 15:15:30 +0800

    jsasignment1.js

--- Modules/04-JS/Assigments/jsasignment1.js
+++ Modules/04-JS/Assigments/jsasignment1.js
@@ -1,11 +1,27 @@
-function sum(array) {
-  let total = 0;
-  for (let i = 0; i < array.length; i++) {
-    total = total + array[i];
+function weeklySalaryProblem(array) {
+  let weekdaySalary = 0;
+  let weekendSalary = 0;
+  for (let i = 0; i < 7; i++) {
+    if (i < 5) {
+      if (array[i] >= 0 && array[i] <= 8) {
+        weekdaySalary = weekdaySalary + array[i] * 10;
+      } else if (array[i] > 8) {
+        weekdaySalary = weekdaySalary + (80 + (array[i] - 8) * 15);
+      }
+    } else {
+      if (array[i] >= 0 && array[i] <= 8) {
+        weekendSalary = weekendSalary + array[i] * 20;
+      } else if (array[i] > 8) {
+        weekendSalary = weekendSalary + (160 + (array[i] - 8) * 30);
+      }
+    }
   }
-  return total;
+  return weekdaySalary + weekendSalary;
 }
-const array1 = [1, 2, 3, 4];
-const array2 = [-3, 5, 19, -6];
-console.log(sum(array1));
-console.log(sum(array2));
+
+let workingHours1 = [8, 8, 8, 8, 8, 0, 0];
+console.log(weeklySalaryProblem(workingHours1));
+let workingHours2 = [10, 10, 10, 0, 8, 0, 0];
+console.log(weeklySalaryProblem(workingHours2));
+let workingHours3 = [0, 0, 0, 0, 0, 12, 0];
+console.log(weeklySalaryProblem(workingHours3));
