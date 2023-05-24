3ce7f17983348417263ce591c48e6417ec70ddf3
ChowYueFong <122257040+ChowYueFong@users.noreply.github.com>
2023-05-24 15:16:26 +0800

    Delete jsasignment4.js

--- Modules/04-JS/Assigments/jsasignment4.js
+++
@@ -1,8 +0,0 @@
-function tetraCalc(x) {
-  let total = 0;
-  total = (x * (x + 1) * (x + 2)) / 6;
-  return total;
-}
-console.log(tetraCalc(2));
-console.log(tetraCalc(5));
-console.log(tetraCalc(6));
